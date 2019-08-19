94a0b4b4826d4321721fc41ee941a156d4a35c57
Nam Nguyen <nguyen.nam@hubblehome.com>
2019-08-19 21:00:29 +0700

    Update Splash screen

--- app/src/main/java/vn/ts5/tattoo/ui/activity/SplashActivity.java
+++ app/src/main/java/vn/ts5/tattoo/ui/activity/SplashActivity.java
@@ -3,6 +3,7 @@ package vn.ts5.tattoo.ui.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,18 +12,19 @@ import vn.ts5.tattoo.R;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private final int SECOND_DELAY = 5;
+    private final int SECOND_DELAY = 1000 * 3; //3 seconds
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splash);
         Handler handler = new Handler();
-        handler.postDelayed(()-> changeToRegisterActivity(), SECOND_DELAY);
+        handler.postDelayed(() -> changeToRegisterActivity(), SECOND_DELAY);
     }
 
     private void changeToRegisterActivity() {
-        startActivity(new Intent(SplashActivity.this,RegisterActivity.class));
+        startActivity(new Intent(SplashActivity.this, RegisterActivity.class));
         finish();
     }
 }
