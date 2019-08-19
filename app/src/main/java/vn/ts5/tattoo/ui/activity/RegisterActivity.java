9b724844c2dc3e2c1613b981badd02f1e920487c
Nam Nguyen <nguyen.nam@hubblehome.com>
2019-08-19 16:24:45 +0700

    Create main class

--- app/src/main/java/vn/ts5/tattoo/ui/activity/RegisterActivity.java
+++ app/src/main/java/vn/ts5/tattoo/ui/activity/RegisterActivity.java
@@ -1,14 +1,16 @@
-package vn.ts5.tattoo;
+package vn.ts5.tattoo.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import vn.ts5.tattoo.R;
+
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
     }
 }
