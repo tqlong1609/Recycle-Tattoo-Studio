00b5e03494984f6f558acc3d8b79553c20b8e633
Long <tqlong1609@gmail.com>
2019-08-19 19:00:11 +0700

    push v1

--- app/src/main/java/vn/ts5/tattoo/ui/activity/SplashActivity.java
+++ app/src/main/java/vn/ts5/tattoo/ui/activity/SplashActivity.java
@@ -2,15 +2,26 @@ package vn.ts5.tattoo.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import vn.ts5.tattoo.R;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private final int SECOND_DELAY = 5;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        Handler handler = new Handler();
+        handler.postDelayed(() -> run(),SECOND_DELAY);
+    }
+
+    private void run() {
+        startActivity(new Intent(SplashActivity.this,RegisterActivity.class));
+        finish();
     }
 }
