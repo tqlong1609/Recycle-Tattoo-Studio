fbfa4d5d1664d653189a5bd2324238ed251c94e9
Long <tqlong1609@gmail.com>
2019-08-22 22:33:30 +0700

    format money

--- app/src/main/java/vn/ts5/tattoo/ui/activity/AddActivity.java
+++ app/src/main/java/vn/ts5/tattoo/ui/activity/AddActivity.java
@@ -5,12 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+
 
 import vn.ts5.tattoo.R;
+import vn.ts5.tattoo.utils.FormatMoneyUtil;
 
 public class AddActivity extends AppCompatActivity {
 
     private Button mBtnConfirm;
+
+    private EditText mEditPrice;
+    private EditText mEditDeposit;
+    private EditText mEditPayment;
+    private EditText mEditBalance;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +27,27 @@ public class AddActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add);
         setupViews();
 
+        formatEditMoney();
+
         mBtnConfirm.setOnClickListener((v) -> startActivity(
                 new Intent(AddActivity.this,RatingActivity.class)));
-
     }
+
+    private void formatEditMoney() {
+        mEditPrice.addTextChangedListener(FormatMoneyUtil.onTextChangedListener(mEditPrice));
+        mEditDeposit.addTextChangedListener(FormatMoneyUtil.onTextChangedListener(mEditDeposit));
+        mEditPayment.addTextChangedListener(FormatMoneyUtil.onTextChangedListener(mEditPayment));
+        mEditBalance.addTextChangedListener(FormatMoneyUtil.onTextChangedListener(mEditBalance));
+    }
+
 
     private void setupViews() {
-        mBtnConfirm = findViewById(R.id.btn_confirm);
+        mBtnConfirm     = findViewById(R.id.btn_confirm);
+        mEditPrice      = findViewById(R.id.edit_price);
+        mEditDeposit    = findViewById(R.id.edit_deposit);
+        mEditPayment    = findViewById(R.id.edit_payment);
+        mEditBalance    = findViewById(R.id.edit_balance);
     }
+
+
 }
--- app/src/main/java/vn/ts5/tattoo/ui/activity/EditActivity.java
+++ app/src/main/java/vn/ts5/tattoo/ui/activity/EditActivity.java
@@ -3,14 +3,37 @@ package vn.ts5.tattoo.ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import vn.ts5.tattoo.R;
+import vn.ts5.tattoo.utils.FormatMoneyUtil;
 
 public class EditActivity extends AppCompatActivity {
+
+    private EditText mEditPrice;
+    private EditText mEditDeposit;
+    private EditText mEditPayment;
+    private EditText mEditBalance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+        setupViews();
+        formatEditMoney();
+    }
+
+    private void formatEditMoney() {
+        mEditPrice.addTextChangedListener(FormatMoneyUtil.onTextChangedListener(mEditPrice));
+        mEditDeposit.addTextChangedListener(FormatMoneyUtil.onTextChangedListener(mEditDeposit));
+        mEditPayment.addTextChangedListener(FormatMoneyUtil.onTextChangedListener(mEditPayment));
+        mEditBalance.addTextChangedListener(FormatMoneyUtil.onTextChangedListener(mEditBalance));
+    }
+
+    private void setupViews() {
+        mEditPrice      = findViewById(R.id.edit_price);
+        mEditDeposit    = findViewById(R.id.edit_deposit);
+        mEditPayment    = findViewById(R.id.edit_payment);
+        mEditBalance    = findViewById(R.id.edit_balance);
     }
 }
