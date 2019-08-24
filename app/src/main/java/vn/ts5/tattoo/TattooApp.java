7a923e91e8d41b11e28949e5a3c0e83296868730
Nam Nguyen <nguyen.nam@hubblehome.com>
2019-08-24 17:47:32 +0700

    Set font , display gif ( not completed yet)

---
+++ app/src/main/java/vn/ts5/tattoo/TattooApp.java
@@ -0,0 +1,20 @@
+package vn.ts5.tattoo;
+
+import android.app.Application;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+
+/**
+ * Created by Nani on 2019-08-24.
+ */
+public class TattooApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath("fonts/Roboto-Regular.ttf")
+                .setFontAttrId(R.attr.fontPath)
+                .build()
+        );
+    }
+}
--- app/src/main/java/vn/ts5/tattoo/ui/activity/BaseActivity.java
+++ app/src/main/java/vn/ts5/tattoo/ui/activity/BaseActivity.java
@@ -1,16 +1,23 @@
 package vn.ts5.tattoo.ui.activity;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import butterknife.ButterKnife;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 /**
  * Created by Nani on 2019-08-24.
  */
 public class BaseActivity extends AppCompatActivity {
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
